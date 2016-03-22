07af5090e28697cad4b6d975559ec1e2ea8bddde
Prawnkus <prawnkus@gmail.com>
2016-03-22 12:57:24 +0100

    Button changes color when looked at and door opens when pushed

--- MED6/Assets/Scripts/Interact.cs
+++ MED6/Assets/Scripts/Interact.cs
@@ -5,18 +5,25 @@ public class Interact : MonoBehaviour {
 	
 	public bool interactableByPlayer = true;
 	public bool activated;
-    private Renderer rend;
+    public Renderer rend;
+	public GameObject linkedDoor;
+
 
 
     void Start()
     {
         rend = GetComponent<Renderer>();
-        rend.enabled = true;
         rend.material.color = Color.white;
     }
 
+
+
 	public void OnLookEnter(){
-		// change color here?
+		rend.material.color = Color.green;
+	}
+
+	public void OnLookExit(){
+		rend.material.color = Color.white;
 	}
 
 	public void interact()
@@ -24,14 +31,22 @@ public class Interact : MonoBehaviour {
 //		Debug.Log ("interacted");
         if (!activated)
         {
-            rend.material.color = Color.green;
             activated = true;
+			OpenDoor ();
         }
         else
         {
-            rend.material.color = Color.white;
+            //rend.material.color = Color.white;
             activated = false;
+			CloseDoor ();
         }
+	}
+
+	public void OpenDoor(){
+		linkedDoor.transform.position += new Vector3 (0, 5, 0);
+	}
+	public void CloseDoor(){
+		linkedDoor.transform.position -= new Vector3 (0, 5, 0);
 	}
 	
 }
--- MED6/Assets/Scripts/RaycastInteract.cs
+++ MED6/Assets/Scripts/RaycastInteract.cs
@@ -4,6 +4,11 @@ using System.Collections;
 public class RaycastInteract : MonoBehaviour {
 
 	public RaycastHit hit;
+	private GameObject lastLookedAt;
+	Interact currInteractScript;
+	Interact prevInteractScript;
+
+
 	
 	
 	void Start () {
@@ -14,10 +19,17 @@ public class RaycastInteract : MonoBehaviour {
 	void Update () {
 		Ray ray = Camera.main.ScreenPointToRay (new Vector3 (Screen.width / 2, Screen.height / 2));
 
-		if(Physics.Raycast(ray,out hit, 3)){
-			// change color here (OnLookEnter, in 'Interact' script?)
-			if(Input.GetKeyDown(KeyCode.E)){
-				hit.collider.gameObject.GetComponent<Interact> ().interact();
+		if (Physics.Raycast (ray, out hit, 3) && hit.collider.gameObject.GetComponent<Interact> () != null) {
+
+			lastLookedAt = hit.collider.gameObject;
+			lastLookedAt.GetComponent<Interact> ().OnLookEnter ();
+
+			if (Input.GetKeyDown (KeyCode.E)) {
+				hit.collider.gameObject.GetComponent<Interact> ().interact ();
+			}
+		} else {
+			if (lastLookedAt != null) {
+				lastLookedAt.GetComponent<Interact> ().OnLookExit ();
 			}
 		}
 	}
