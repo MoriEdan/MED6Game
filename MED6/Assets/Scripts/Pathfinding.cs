cd983dd472edc9ecf76cb8805bc161a9e87c1ff8
WishPotato <wish92jfn@hotmail.com>
2016-03-23 12:42:43 +0100

    Basic pathfinding - Ready
    Moves object within the navmesh to its final destination

--- MED6/Assets/Scripts/Pathfinding.cs
+++ MED6/Assets/Scripts/Pathfinding.cs
@@ -4,13 +4,15 @@ using System.Collections;
 public class Pathfinding : MonoBehaviour {
 
     private NavMeshAgent agent;
+    private GameObject goTo;
 
 	void Start () {
         agent = GetComponent<NavMeshAgent>();
+        goTo = GameObject.Find("GoTo");
 	}
 	
 	
 	void Update () {
-	    
+        agent.SetDestination(goTo.transform.position);
 	}
 }
