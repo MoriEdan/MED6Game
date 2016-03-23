949a6cf18a1b363177b8ce75b93db41a99926ca3
WishPotato <wish92jfn@hotmail.com>
2016-03-23 11:10:24 +0100

    Start of a new Path
    AI Pathfinding work scene setup and on its way to something.

---
+++ MED6/Assets/Scripts/Pathfinding.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pathfinding : MonoBehaviour {
+
+    private NavMeshAgent agent;
+
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	
+	void Update () {
+	    
+	}
+}
