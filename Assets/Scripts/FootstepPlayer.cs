923a1eb276aed6cc29ff9da6c8cae37178b89136
Prawnkus <prawnkus@gmail.com>
2016-03-31 11:10:24 +0200

    Level properly set up. Player character re-made
    Still needs button to be linked to an object.
    This button is however just a placeholder.

---
+++ Assets/Scripts/FootstepPlayer.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootstepPlayer : MonoBehaviour {
+	private RaycastHit hit = new RaycastHit();
+	private bool walkCheck;
+	private bool isRPlaying, isAnythingPlaying;
+
+	private GameObject[] RData;
+	private int d8, d8old;
+
+void Start () {
+
+	RData = new GameObject[8];
+
+	//Rock
+	RData[0] = GameObject.Find ("R1");
+	RData[1] = GameObject.Find ("R2");
+	RData[2] = GameObject.Find ("R3");
+	RData[3] = GameObject.Find ("R4");
+	RData[4] = GameObject.Find ("R5");
+	RData[5] = GameObject.Find ("R6");
+	RData[6] = GameObject.Find ("R7");
+	RData[7] = GameObject.Find ("R8");
+
+}
+
+
+void Update () {
+		walkCheck = GameObject.Find ("Player").GetComponent<PlayerMovement> ().Walking;
+
+		//Checks if player is walking
+		if (walkCheck) {
+			
+			d8old = d8;
+			while (d8 == d8old) {
+				d8 = Random.Range (0, 7);
+			}
+
+			isRPlaying = false;
+
+			//Checks if there is a surface beneath the player
+			if (walkCheck && Physics.Raycast (transform.position, -Vector3.up, out hit, 3)) {
+
+				if (isAnythingPlaying == false) {
+					//Stops all sounds, plays a footstep, then starts playing a new footstep every .5 sec
+					switch (hit.collider.gameObject.tag) {
+					case "Stone Floor":
+						isRPlaying = true;
+						ShutUp ();
+						RData [d8].GetComponent<AudioSource> ().Play ();
+						RData [d8].GetComponent<AudioSource> ().volume = 1;
+						StartCoroutine (RPlayer ());
+						break;
+					default:
+						break;
+					}
+					isAnythingPlaying = true;
+				}
+			}
+		}
+	}
+
+
+//Plays a footstep after a .5 sec wait
+private IEnumerator RPlayer(){
+	yield return new WaitForSeconds (0.5f);
+	if (isRPlaying) {
+		RData[d8].GetComponent<AudioSource>().Play ();
+	}
+	isRPlaying = false;
+	isAnythingPlaying = false;
+}
+
+//Stops all sounds
+void ShutUp(){
+	for(int i=0; i<RData.Length; i++){
+		RData[i].GetComponent<AudioSource>().volume-=0.4f;
+	}
+}
+
+}
---
+++ Assets/Scripts/RaycastInteract.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RaycastInteract : MonoBehaviour {
+
+	private float interactReach = 2.5f;
+
+	public RaycastHit hit;
+	private GameObject lastLookedAt;
+	Interact currInteractScript;
+	Interact prevInteractScript;
+
+	private GameObject[] Buttons;
+	
+	void Start () {
+		Buttons = GameObject.FindGameObjectsWithTag ("Button");
+	}
+	
+	
+	void Update () {
+		//Ray ray = Camera.main.ScreenPointToRay (new Vector3 (Screen.width / 2, Screen.height / 2));
+
+		for(int i = 0; i<Buttons.Length;i++){
+			if(Vector3.Angle (Buttons [i].transform.position - transform.position, transform.forward)<30 && 
+				Vector3.Magnitude(Buttons [i].transform.position-transform.position)<interactReach){
+
+				lastLookedAt = Buttons [i];
+				lastLookedAt.GetComponent<Interact> ().OnLookEnter ();
+
+				if (Input.GetKeyDown (KeyCode.E)) {
+					Buttons [i].GetComponent<Interact> ().interact ();
+				}
+				} else {
+					if (lastLookedAt != null) {
+						Buttons[i].GetComponent<Interact> ().OnLookExit ();
+					}
+				}
+			}
+		}
+	}
