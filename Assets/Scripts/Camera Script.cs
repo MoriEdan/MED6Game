aa0a9b6b1360510b9a5d8c7d5da44fd912261b3e
WishPotato <wish92jfn@hotmail.com>
2016-04-16 13:24:54 +0200

    Start of a new Begining

---
+++ Assets/Scripts/Camera Script.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+    private GameObject player;
+
+	void Start () {
+        if (GameObject.FindGameObjectWithTag("Player") == null)
+        {
+            Debug.Log("Error: Cannot find a Gameobject with the tag: Player");
+        }
+        else
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+	}
+
+	void Update () {
+        this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10.0f);
+	}
+}
