86681dd15692be0ce6c444bf1ff631f16faa66de
WishPotato <wish92jfn@hotmail.com>
2016-05-03 15:10:55 +0200

    Work for 3rd May
    Adjusting spawn point, under development.

---
+++ Assets/Scripts/SpawnPointAdjustmentScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPointAdjustmentScript : MonoBehaviour {
+
+    public GameObject[] spawnVectors;
+
+    private PlayerScript player;
+    private bool errorHasOccured;
+
+	void Awake () {
+        if (spawnVectors.Length > 0)
+        {
+            for (int i = 0; i < spawnVectors.Length; i++)
+            {
+                if (spawnVectors[i].GetComponent<SpawnVectorScript>() == null)
+                {
+                    Debug.Log("SpawnVector GameObject No." + i + " does not have the required script Attached.");
+                    errorHasOccured = true;
+                    break;
+                }
+            }
+            if (!errorHasOccured)
+            {
+                for (int i = 0; i < spawnVectors.Length - 1; i++)
+                {
+                    spawnVectors[i].GetComponent<SpawnVectorScript>().Direction(spawnVectors[i + 1].transform.position);
+                }
+            }
+            else
+                Debug.Log("Fix the error before continuing playing the game.");
+        }
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerScript>();
+	}
+
+	void FixedUpdate () {
+        if (!player.isAlive)
+        {
+            for (int i = 0; i < spawnVectors.Length; i++)
+            {
+                if (spawnVectors[i].GetComponent<SpawnVectorScript>().hasPassed)
+                    continue;
+                else
+                {
+                    // New spawn point will be set.
+                    break;
+                }
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/SpawnVectorScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnVectorScript : MonoBehaviour {
+
+	public bool hasPassed;
+
+    private GameObject playerObj;
+    private float direction = 1.0f;
+
+	void Start () {
+        playerObj = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	void FixedUpdate () {
+        if (!hasPassed)
+        {
+            if (direction > 0.1f)
+            {
+                if (playerObj.transform.position.x >= this.transform.position.x && Vector3.Distance(this.transform.position, playerObj.transform.position) <= 5.0f)
+                    hasPassed = true;
+            }
+            else if (direction < -0.1f)
+            {
+                if (playerObj.transform.position.x >= this.transform.position.x && Vector3.Distance(this.transform.position, playerObj.transform.position) <= 5.0f)
+                    hasPassed = true;
+            }
+        }
+	}
+
+    public float Direction(Vector3 a)
+    {
+        direction = Vector3.Normalize(this.transform.position - a).x;
+        Debug.Log(direction);
+        return direction;
+    }
+}
